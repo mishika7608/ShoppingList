6a0cc163904920f15d05672f9f624ee8251e9082
mishika7608 <csds23040@glbitm.ac.in>
2025-07-23 16:55:00 +0530

    Set item to edit

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
 const items = itemList.querySelectorAll('li');
+const formBtn = itemForm.querySelector('button');
+let isEditMode = false;
 
 function displayItems(){
     const itemsFromStorage = getItemsFromStorage();
@@ -77,7 +79,20 @@ function onClickItem(e){
     if (e.target.parentElement.classList.contains('remove-item')){
         removeItem(e.target.parentElement.parentElement);
     }
+    else{
+        setItemToEdit(e.target);
+    }
 }
+
+function setItemToEdit(item){
+    isEditMode = true;
+    itemList.querySelectorAll('li').forEach((i) => i.classList.remove('edit-mode'));
+    item.classList.add('edit-mode');
+    formBtn.innerHTML = '<i class="fa-solid fa-pen></i> Update Item';
+    formBtn.style.backgroundColor = '#228B22';
+    itemInput.value = item.textContent;
+}
+
 function removeItem(item){
         if (confirm('Are you sure?')){
             //Remove Item from DOM
