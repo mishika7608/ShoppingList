5809162d637614670522caae237a13130a6759ef
mishika7608 <csds23040@glbitm.ac.in>
2025-07-23 12:16:31 +0530

    add button in new items

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 
+function createIcon(classes){
+    const icon = document.createElement('i');
+    icon.className = classes;
+    return icon;
+}
+
+function createButton(classes){
+    const button = document.createElement('button')
+    button.className = classes;
+    const icon = createIcon('fa-solid fa-xmark');
+    button.appendChild(icon);
+    return button;
+}
+
+
 function addItem(e){
     e.preventDefault();
     const newItem =itemInput.value;
@@ -14,7 +29,9 @@ function addItem(e){
     const li = document.createElement('li');
     li.appendChild(document.createTextNode(newItem));
     console.log(li);
-
+    
+    const button = createButton('remove-item btn-link text-red');
+    li.appendChild(button);
     itemList.appendChild(li);
     itemInput.value='';
 }
