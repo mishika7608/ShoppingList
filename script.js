c8624a4a2435f58b21e339234f5dce9f4bd89630
mishika7608 <csds23040@glbitm.ac.in>
2025-07-23 13:31:02 +0530

    Logging results to console for testing

--- script.js
+++ script.js
@@ -55,6 +55,21 @@ function clearItems(){
     checkUI();
 }
 
+function filterItems(){
+    const text = e.target.value.toLowerCase();
+    const items = itemList.querySelectorAll('li');
+    items.forEach(item => {
+        const itemName = item.firstChild.textContent.toLowerCase();
+        if (itemName.indexOf(text) != -1){ 
+            console.log(true);
+        }
+        else{
+            console.log(false);
+        }
+    });
+    console.log(text);
+}
+
 function checkUI(){
     const items = itemList.querySelectorAll('li'); 
     if (items.length === 0){
@@ -71,4 +86,5 @@ function checkUI(){
 itemForm.addEventListener('submit',addItem);
 itemList.addEventListener('click',removeItem)
 clearBtn.addEventListener('click',clearItems)
+itemFilter.addEventListener('input',filterItems)
 checkUI();
