d56a1db042d23e1453d8fea9961e824e7fd596be
mishika7608 <csds23040@glbitm.ac.in>
2025-07-23 13:09:31 +0530

    Toggle clear and filter UI based on item count

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+const items = itemList.querySelectorAll('li');
 
 function createIcon(classes){
     const icon = document.createElement('i');
@@ -34,11 +36,15 @@ function addItem(e){
     li.appendChild(button);
     itemList.appendChild(li);
     itemInput.value='';
+    checkUI();
 }
 
 function removeItem(e){
     if (e.target.parentElement.classList.contains('remove-item')){
-        e.target.parentElement.parentElement.remove();
+        if (confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove();
+            checkUI();
+        }
     }
 }
 
@@ -46,9 +52,23 @@ function clearItems(){
     while (itemList.firstChild){
         itemList.removeChild(itemList.firstChild);
     }
+    checkUI();
+}
+
+function checkUI(){
+    const items = itemList.querySelectorAll('li'); 
+    if (items.length === 0){
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';
+    }
+    else{
+        clearBtn.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
 }
 
 //Event Listener
 itemForm.addEventListener('submit',addItem);
 itemList.addEventListener('click',removeItem)
 clearBtn.addEventListener('click',clearItems)
+checkUI();
